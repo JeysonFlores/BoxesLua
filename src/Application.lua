cb4dec764fb26a93ef6120f9c66b2a0e92011c78
JeysonFlores <jeyson.flores@protonmail.com>
2021-09-12 14:26:50 -0600

    Icons Added

--- src/Application.lua
+++ src/Application.lua
@@ -9,50 +9,56 @@ local Gdk = lgi.require('Gdk')
 
 require "src.MainWindow"
 
-app = Gtk.Application {
-    application_id = "com.github.jeysonflores.boxes"
-}
+Application = {}
 
-
-function app:on_activate()
-    main_window = MainWindow.new()
-    main_window.application = self
-
-    local settings = Gio.Settings {
-        schema_id = "com.github.jeysonflores.boxes"
+function Application.new()
+    app = Gtk.Application {
+        application_id = "com.github.jeysonflores.boxes"
     }
 
-    screen = Gdk.Screen.get_default()
-    provider = Gtk.CssProvider{}
-    
-    local granite_settings = Granite.Settings{}
-    local gtk_settings = Gtk.Settings:get_default()
-    
 
-    if granite_settings.prefers_color_scheme == "DARK" then
-        gtk_settings.gtk_application_prefer_dark_theme = true
-        provider:load_from_path("/home/jeyson/Proyectos/Lua/Blossom/data/style-dark.css")
-        Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-    else
-        gtk_settings.gtk_application_prefer_dark_theme = false
-        provider:load_from_path("/home/jeyson/Proyectos/Lua/Blossom/data/style.css")
-        Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-    end
+    function app:on_activate()
+        main_window = MainWindow.new()
+        main_window.application = self
 
-    granite_settings.on_notify["prefers-color-scheme"] = function(self, pspec)
+        local settings = Gio.Settings {
+            schema_id = "com.github.jeysonflores.boxes"
+        }
+
+        screen = Gdk.Screen.get_default()
+        provider = Gtk.CssProvider{}
+        
+        local granite_settings = Granite.Settings{}
+        local gtk_settings = Gtk.Settings:get_default()
+        
+
         if granite_settings.prefers_color_scheme == "DARK" then
             gtk_settings.gtk_application_prefer_dark_theme = true
-            provider:load_from_path("/home/jeyson/Proyectos/Lua/Blossom/data/style-dark.css")
+            provider:load_from_path("/home/jeyson/Proyectos/Lua/Boxes/data/style-dark.css")
             Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
         else
             gtk_settings.gtk_application_prefer_dark_theme = false
-            provider:load_from_path("/home/jeyson/Proyectos/Lua/Blossom/data/style.css")
+            provider:load_from_path("/home/jeyson/Proyectos/Lua/Boxes/data/style.css")
             Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
         end
+
+        granite_settings.on_notify["prefers-color-scheme"] = function(self, pspec)
+            if granite_settings.prefers_color_scheme == "DARK" then
+                gtk_settings.gtk_application_prefer_dark_theme = true
+                provider:load_from_path("/home/jeyson/Proyectos/Lua/Boxes/data/style-dark.css")
+                Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+            else
+                gtk_settings.gtk_application_prefer_dark_theme = false
+                provider:load_from_path("/home/jeyson/Proyectos/Lua/Boxes/data/style.css")
+                Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+            end
+        end
+
+        main_window:resize(settings:get_int("window-width"), settings:get_int("window-height"))
+        main_window:move(settings:get_int("pos-x"), settings:get_int("pos-y"))
+        
+        main_window:show_all()
     end
 
-    main_window:resize(settings:get_int("window-width"), settings:get_int("window-height"))
-    main_window:move(settings:get_int("pos-x"), settings:get_int("pos-y"))
-    
-    main_window:show_all()
+    return app
 end
